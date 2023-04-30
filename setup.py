7352792a765259b3f4f59f3c55127f4e4ae1a921
Zhymabek Roman <61125068+ZhymabekRoman@users.noreply.github.com>
2023-04-30 14:31:09 +0600

    update: update version

--- setup.py
+++ setup.py
@@ -6,10 +6,9 @@ assert sys.version_info[0] == 3, "Platonus API Wrapper requires Python 3.x"
 
 setup(
     name='platonus_api_wrapper',
-    version='2021.05.11',
+    version='30.04.2023',
     description='Platonus API Wrapper - неофициальная обертка для работы с Платонусом',
     author="Zhymabek Roman",
-    author_email="robanokssamit@yandex.ru",
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     url='https://github.com/ZhymabekRoman/platonus-api-wrapper',
     setup_requires=['wheel'],
